2cc9980a15dee15e206da8fa6872816bc7934d55
Antoine Colmard <a.colmard@gmail.com>
2015-03-14 00:10:54 +0100

    Created project structure with basic angular application.

---
+++ src/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+use Slim\Slim;
+
+// Create the application
+$app = new Slim(array(
+    'templates.path' => __DIR__ . '/../web'
+));
+
+$app->config('debug', true);
+
+// Error handling
+$app->notFound(function () use ($app) {
+    $app->render('404.html');
+});
+
+$app->error(function (\Exception $ex) use ($app) {
+    $app->getLog()->alert($ex);
+});
+
+return $app;
